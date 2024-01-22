4e18e7dfd1f1caa44ca8b620298f2615b44d660f
berlaypott143 <157326628+berlaypott143@users.noreply.github.com>
2024-01-22 08:26:13 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// Calculator Program
+
+const display = document.getElementById('display');
+
+const appendToDisplay = (input) => {
+    display.value += input;
+} 
+
+const clearDisplay = () => {
+    display.value = "";
+}
+
+const calculate = () => {
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error) {
+        display.value = "ERROR!";
+    }
+}
